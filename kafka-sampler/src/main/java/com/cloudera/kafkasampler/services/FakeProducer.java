8b660e1d32f23bb622cde3fbc7b1cfcaa6cdd21d
Tibor Meller <tibor.meller@gmail.com>
2020-02-08 21:19:01 +0100

    [] Extending porject, adding initail kafka consumer and a fake producer

---
+++ kafka-sampler/src/main/java/com/cloudera/kafkasampler/services/FakeProducer.java
@@ -0,0 +1,22 @@
+package com.cloudera.kafkasampler.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FakeProducer {
+
+    private static final Logger logger = LoggerFactory.getLogger(FakeProducer.class);
+    private static final String TOPIC = "users";
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String message) {
+        logger.info(String.format("#### -> Producing message -> %s", message));
+        this.kafkaTemplate.send(TOPIC, message);
+    }
+}
